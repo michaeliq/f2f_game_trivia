8b78e10dceba596342860eface8f789b0f5d30a6
Advance Servicios Integrales <122900399+ADVANCESAS@users.noreply.github.com>
2024-02-19 01:49:15 -0500

    add, new components. update, components point and compatitor

--- src/components/ContainerCompatitor.jsx
+++ src/components/ContainerCompatitor.jsx
@@ -1,19 +1,24 @@
-import module_compatitor_bottom from "../resources/modulo_competidor_abajo.png"
-import module_compatitor_top from "../resources/modulo_competidor_arriba.png"
-import module_point_right from "../resources/modulo_puntos_der.png"
-import module_point_left from "../resources/modulo_puntos_izq.png"
+import CompatitorGame from "./CompatitiorGame"
+
 import "../styles/components/ContainerCompatitorGame.css"
+import PointsGame from "./PointsGame"
 
 const ContainerCompatitorGame = () => {
     return (
         <div className="container-compatitor-game">
-            <img src={module_compatitor_top} className="module-compatitor group-a" alt="Competidor 1 equipo A" />
-            <img src={module_compatitor_top} className="module-compatitor group-b" alt="Competidor 1 equipo B" />
-            <img src={module_point_left} className="module-point-left" alt="Puntos equipo A" />
-            <img src={module_compatitor_bottom} className="module-compatitor group-a" alt="Competidor2 equipo A" />
-            <img src={module_compatitor_bottom} className="module-compatitor group-b" alt="Competidor 2 equipo B" />
-            <img src={module_point_right} className="module-point-right" alt="Puntos equipo B" />
-
+            <CompatitorGame axisX={"right"} axisY={"Top"} altText={"Competidor 1 equipo A"}>
+                <p>Jugador 1</p>
+            </CompatitorGame>
+            <CompatitorGame axisX={"left"} axisY={"Top"} alt="Competidor 1 equipo B">
+                <p>Jugador 2</p>
+            </CompatitorGame>
+            <CompatitorGame axisX={"right"} axisY={"Bottom"} alt="Competidor2 equipo A">
+                <p>Jugador 3</p>
+            </CompatitorGame>
+            <CompatitorGame axisX={"left"} axisY={"Bottom"} alt="Competidor 2 equipo B" >
+                <p>Jugador 4</p>
+            </CompatitorGame>
+            <PointsGame/>
         </div>
     )
 }
--- src/components/ContainerGame.jsx
+++ src/components/ContainerGame.jsx
@@ -1,10 +1,14 @@
 import background_img_ring from "../resources/background_image.png" 
+import logo from "../resources/f2f_challenge_logo.png"
 import "../styles/components/ContainerGame.css"
+import Time from "./Time"
 const ContainerGame = ({children}) => {
 
     return(
         <div className="container-game">
             <img src={background_img_ring} className="background-img-game" alt="Imagen del ring"/>
+            <img src={logo} className="logo-img-game" alt="Imagen del logo"/>
+            <Time/>
             {children}
         </div>
     )
